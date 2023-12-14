d05d0ec17b30247714d924965f56373d14127f06
Padmaprasath <padmaprasath33@gmail.com>
2023-12-14 16:26:32 +0530

    Added ECS config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Copy the index.html file to the EFS volume
-#cp /app/index.html /usr/share/nginx/html/index.html
+cp /app/index.html /usr/share/nginx/html/index.html
 
 # Copy the index.html file from the root EFS volume directory to the desired mount path
-cp /index.html /usr/share/nginx/html/index.html
+cp /usr/share/nginx/html/index.html /usr/share/nginx/html/
 
 # Continue with the default entrypoint (e.g., starting the Nginx server)
 #exec "$@"
